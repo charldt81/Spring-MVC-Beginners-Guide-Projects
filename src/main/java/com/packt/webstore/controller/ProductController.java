3677d91c4c75146cd72ae9702982572dc1c0626a
Charl Du Toit <charldt81@gmail.com>
2018-05-22 13:26:46 +0200

    This is the commit for Chapter_2 - Go Hero Exercise 1

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -5,18 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.packt.webstore.domain.repository.ProductRepository;
 import com.packt.webstore.service.ProductService;
 
 
-//to put the product information in a model, we needed to create this controller class ProductController{}... step3
+// to put the product information in a model, we needed to create this controller class ProductController{}... step3
 
 @Controller
 public class ProductController {
 	
-	
-	@Autowired
-	private ProductRepository productRepository;
 	
 	@Autowired
 	private ProductService productService;
@@ -26,7 +22,7 @@ public class ProductController {
 	// Apple's iPhone 5s and add that object to the model.
 	@RequestMapping("/products")
 	public String list(Model model) {
-		model.addAttribute("products", productRepository.getAllProducts());
+		model.addAttribute("products", productService.getAllProducts());
 		return "products";
 		// and finally we return the view name as 'products'
 	}
--- src/main/java/com/packt/webstore/service/ProductService.java
+++ src/main/java/com/packt/webstore/service/ProductService.java
@@ -1,10 +1,16 @@
 package com.packt.webstore.service;
 
+import java.util.List;
+
+import com.packt.webstore.domain.Product;
 
 
 
 public interface ProductService {
 
+	
+	List <Product> getAllProducts();
+	
 	void updateAllStock();
 	
 	
